20a66ad7b3b54eadf0e5cee7ad42c6ed85756cb0
孙道然 <513088699@qq.com>
2016-09-30 15:12:00 +0800

    add images

--- Podfile
+++ Podfile
@@ -1,6 +1,6 @@
 platform :ios, '7.0'
 target "sun_bannerView" do
-    #pod "SDWebImage"
+    pod "SDWebImage"
 end
  
 
---
+++ Pods/Target Support Files/Pods-sun_bannerView/Pods-sun_bannerView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sun_bannerView : NSObject
+@end
+@implementation PodsDummy_Pods_sun_bannerView
+@end
